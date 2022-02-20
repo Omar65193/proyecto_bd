4aa4b77124abd3877220a5d0fb2b0ed8109327df
Omar65193 <playstation.nintendo@outlook.com>
2022-02-20 10:20:41 -0600

    agregado el nuevo proeycto

---
+++ nuevo/nuevo/Proyecto2/MostrarLibros.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Proyecto2
+{
+    public partial class Contact : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnMostrar_Click(object sender, EventArgs e)
+        {
+            DAOLibros dAOLibros = new DAOLibros();
+            gvLibros.DataSource = dAOLibros.select();
+            gvLibros.DataBind();
+        }
+
+        protected void btnAgregar_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
